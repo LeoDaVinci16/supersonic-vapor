9c21836918a01c60d40690064c5c4c91b70ca5a5
Arnau Coronado Nadal <arnaucnadal@disroot.org>
2026-03-17 13:09:31 +0100

    canvis en el diccionari pero encara queda feina per fer

--- src/points_dict.py
+++ src/points_dict.py
@@ -10,7 +10,26 @@ points_dict = {
     "PM-V-9": "Atomitzador 2a planta",
     "PM-V-10": "S600",
     "PM-V-11": "S6000",
-
-    "101": "PEC"
-
+    "STE-1": "E800",
+    "STE-2": "Secadores",
+    "STE-3": "PEC",
+    "STE-4": "Nautas",
+    "STE-5": "V800",
+    "STE-6": "V700",
+    "STE-7": "P400",
+    "STE-8": "Atomitzador 1a planta",
+    "STE-9": "Atomitzador 2a planta",
+    "STE-10": "S600",
+    "STE-11": "S6000",
 }
+
+def new_name(id):
+    return points_dict.get(id, id)
+
+def main(input):
+    name = new_name(input)
+    #print(name)
+    return name
+
+if __name__ == "__main__":
+    main("PM-V-3")
